bc26bcc478014a62eaa345b410732a2ec2b70fb2
JoaoVictorBento <135682833+JoaoVictorBento@users.noreply.github.com>
2023-06-07 14:24:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_do_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Olá! esse é o meu projeto");
+
+            Console.WriteLine("Ele é um teste para o GitHub");
+
+            Console.WriteLine("Nada de novo por aqui...");
+
+            Console.ReadKey();
+        
+        }
+    }
+}
